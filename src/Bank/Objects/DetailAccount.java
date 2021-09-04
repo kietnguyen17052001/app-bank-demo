642cd4fabe8c070d3b74d4052ca0e24c75d61414
kietnguyen17052001 <kietnguyen17052001@gmail.com>
2021-09-04 19:47:39 +0700

    Edit object DetailAccount

--- src/Bank/Objects/DetailAccount.java
+++ src/Bank/Objects/DetailAccount.java
@@ -8,20 +8,18 @@ public class DetailAccount {
 	public LocalDateTime dayTrading;
 	public float transactionAmount;
 	public float balance;
-	public String ATM;
-	public int tradingCreditCardID;
+	public String accountNumber;
 	public String accountName;
 	public String transactionContent;
 
-	public DetailAccount(int creditCardID, String transactionType, float transactionAmount, float balance, String ATM,
-			int tradingCreditCardID, String accountName, String transactionContent) {
+	public DetailAccount(int creditCardID, String transactionType, LocalDateTime dayTrading, float transactionAmount,
+			float balance, String accountNumber, String accountName, String transactionContent) {
 		this.creditCardID = creditCardID;
 		this.transactionType = transactionType;
 		this.dayTrading = LocalDateTime.now();
 		this.transactionAmount = transactionAmount;
 		this.balance = balance;
-		this.ATM = ATM;
-		this.tradingCreditCardID = tradingCreditCardID;
+		this.accountNumber = accountNumber;
 		this.accountName = accountName;
 		this.transactionContent = transactionContent;
 	}
