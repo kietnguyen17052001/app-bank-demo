0553de7c3fed32880af8ad92053bae50ceb9ee53
kietnguyen17052001 <kietnguyen17052001@gmail.com>
2021-09-05 21:10:20 +0700

    Update object Account

--- src/Bank/Objects/Account.java
+++ src/Bank/Objects/Account.java
@@ -3,23 +3,29 @@ package Bank.Objects;
 import java.util.Date;
 
 public class Account {
+	public int creditCardID;
 	public String accountNumber;
 	public String accountName;
 	public float balance;
 	public String phone;
 	public String userID;
 	public String userName;
+	public String password;
+	public String pin;
 	public String bank;
 	public Date dayCreate;
 
-	public Account(String accountNumber, String accountName, float balance, String phone, String userID,
-			String userName, String bank, Date dayCreate) {
+	public Account(int creditCardID, String accountNumber, String accountName, float balance, String phone, String userID,
+			String userName, String password, String pin, String bank, Date dayCreate) {
+		this.creditCardID = creditCardID;
 		this.accountNumber = accountNumber;
 		this.accountName = accountName;
 		this.balance = balance;
 		this.phone = phone;
 		this.userID = userID;
 		this.userName = userName;
+		this.password = password;
+		this.pin = pin;
 		this.bank = bank;
 		this.dayCreate = dayCreate;
 	}
