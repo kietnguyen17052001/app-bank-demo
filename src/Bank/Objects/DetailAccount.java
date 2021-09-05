d6e4b81cc9639843ceeb3fa368c96dba82a59095
kietnguyen17052001 <kietnguyen17052001@gmail.com>
2021-09-06 00:26:20 +0700

    Edit object DetailAccount

--- src/Bank/Objects/DetailAccount.java
+++ src/Bank/Objects/DetailAccount.java
@@ -6,29 +6,29 @@ import java.util.Date;
 public class DetailAccount {
 	public int creditCardID;
 	public int tradingCode;
-	public String accountNumberSender;
+	public String accountNumberFirst;
 	public String transactionType;
 	public Object dayTrading;
 	public float transactionAmount;
 	public float balance;
 	public String ATM;
-	public String accountNumberReceiver;
-	public String accountNameReceiver;
+	public String accountNumberSecond;
+	public String accountNameSecond;
 	public String transactionContent;
 
-	public DetailAccount(int creditCardID, int tradingCode, String accountNumberSender, String transactionType, Object dayTrading,
-			float transactionAmount, float balance, String ATM, String accountNumberReceiver,
-			String accountNameReceiver, String transactionContent) {
+	public DetailAccount(int creditCardID, int tradingCode, String accountNumberFirst, String transactionType, Object dayTrading,
+			float transactionAmount, float balance, String ATM, String accountNumberSecond,
+			String accountNameSecond, String transactionContent) {
 		this.creditCardID = creditCardID;
 		this.tradingCode = tradingCode;
-		this.accountNumberSender = accountNumberSender;
+		this.accountNumberFirst = accountNumberFirst;
 		this.transactionType = transactionType;
 		this.dayTrading = dayTrading;
 		this.transactionAmount = transactionAmount;
 		this.balance = balance;
 		this.ATM = ATM;
-		this.accountNumberReceiver = accountNumberReceiver;
-		this.accountNameReceiver = accountNameReceiver;
+		this.accountNumberSecond = accountNumberSecond;
+		this.accountNameSecond = accountNameSecond;
 		this.transactionContent = transactionContent;
 	}
 }
