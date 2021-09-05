798fdc53f584a37138480142afca1682cf46d99c
kietnguyen17052001 <kietnguyen17052001@gmail.com>
2021-09-05 21:14:25 +0700

    Update object DetailAccount

--- src/Bank/Objects/DetailAccount.java
+++ src/Bank/Objects/DetailAccount.java
@@ -5,21 +5,28 @@ import java.util.Date;
 
 public class DetailAccount {
 	public int creditCardID;
+	public int tradingCode;
 	public String transactionType;
 	public Object dayTrading;
 	public float transactionAmount;
 	public float balance;
+	public String ATM;
+	public int tradingCreditCardID;
 	public String accountNumber;
 	public String accountName;
 	public String transactionContent;
 
-	public DetailAccount(int creditCardID, String transactionType, Object dayTrading, float transactionAmount,
-			float balance, String accountNumber, String accountName, String transactionContent) {
+	public DetailAccount(int creditCardID, int tradingCode, String transactionType, Object dayTrading,
+			float transactionAmount, float balance, String ATM, int tradingCreditCardID, String accountNumber,
+			String accountName, String transactionContent) {
 		this.creditCardID = creditCardID;
+		this.tradingCode = tradingCode;
 		this.transactionType = transactionType;
 		this.dayTrading = dayTrading;
 		this.transactionAmount = transactionAmount;
 		this.balance = balance;
+		this.ATM = ATM;
+		this.tradingCreditCardID = tradingCreditCardID;
 		this.accountNumber = accountNumber;
 		this.accountName = accountName;
 		this.transactionContent = transactionContent;
